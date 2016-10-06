023fa4dddb8dd996d30591c3159f726ed4836d49
Араик Акопян <araikakopan@MacBook-Pro-Araik.local>
2016-10-06 19:08:44 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -9,12 +9,25 @@ require 'sqlite3'
  	@db.results_as_hash = true
  end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы
  before do
+
+ 	# инициализация БД
+ 	
  	init_db
  end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
  configure do
+
+ 	# инициализация БД
+
  	init_db
+
+ 	#создает таблицу Posts
+
  	@db.execute 'CREATE TABLE Posts
  	(
 	 	id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,12 +39,19 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get- запроса /new
+# (барузер получает страницу с сервера)
+
 get '/new' do
   erb :new
 end
 
-post '/new' do
-  content = params[:content]
+# обработчик poct-запроса/new
+# (браузер отправляет данные на сервер)
 
-  erb "You typed #{content}"
+post '/new' do
+	# получаем переменную  из Post запроса
+  	content = params[:content]
+
+  	erb "You typed #{content}"
 end
